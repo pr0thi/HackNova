7a5e354dd6a1780447c170864b8928a2be875940
Harsh Pruthi <68766440+pr0thi@users.noreply.github.com>
2025-10-19 01:39:04 +0530

    StackGuard Auto-Remediation: Secure secrets in src/Components/constants/constants.js

--- src/Components/constants/constants.js
+++ src/Components/constants/constants.js
@@ -1,2 +1,10 @@
+import vault from 'node-vault';
+
+const client = vault({
+    apiVersion: 'v1',
+    endpoint: process.env.VAULT_ADDR,
+    token: process.env.VAULT_TOKEN
+});
+
 export const REACT_APP_GOOGLE_MAPS_KEY =
-  "AIzaSyC0KAWWPur5EZy72zphYs77eeAe4mM_8Ac";
+(await client.read('secret/data/pr0thi-hacknova-src-components-constants-js')).data.data.secret;
